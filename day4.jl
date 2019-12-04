40156dff3f59615597fb48e5457ef762565a0d45
Alex Ames <aames@wisc.edu>
2019-12-04 08:45:13 -0600

    day4 solution

---
+++ day4.jl
@@ -0,0 +1,13 @@
+function checknumber_part1(n)
+    ndigits = digits(n)
+    issorted(ndigits, rev=true) && !allunique(ndigits)
+end
+
+function checknumber_part2(n)
+    ndigits = digits(n)
+    issorted(ndigits, rev=true) && !allunique(ndigits) && any(==(2), sum(unique(ndigits)' .== ndigits, dims=1))
+end
+
+n1, n2 = 284639
+count(n -> checknumber_part1(n), n1:n2)
+count(n -> checknumber_part2(n), n1:n2)
