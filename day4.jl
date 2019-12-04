9487ca5b5542cadabd9314435b0aa006f857423d
Alex Ames <aames@wisc.edu>
2019-12-04 08:55:49 -0600

    cleaned up day4

--- day4.jl
+++ day4.jl
@@ -5,9 +5,11 @@ end
 
 function checknumber_part2(n)
     ndigits = digits(n)
-    issorted(ndigits, rev=true) && !allunique(ndigits) && any(==(2), sum(unique(ndigits)' .== ndigits, dims=1))
+    (issorted(ndigits, rev=true)
+     && !allunique(ndigits)
+     && any(==(2), sum(unique(ndigits)' .== ndigits, dims=1)))
 end
 
-n1, n2 = 284639
+n1, n2 = 284639, 748759
 count(n -> checknumber_part1(n), n1:n2)
 count(n -> checknumber_part2(n), n1:n2)
